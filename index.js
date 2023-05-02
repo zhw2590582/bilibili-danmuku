7365f44c1f5e03d67e94ce97eb7c031cb5344698
Harvey Zack <laozhaochaguan@gmail.com>
2023-05-02 17:03:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+const helmet = require("helmet");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (origin) res.header("Access-Control-Allow-Origin", origin);
+  next();
+});
+
+app.get("/", async function (req, res) {
+  if (!req.query.id) return res.send("Missing query: id");
+  const { data } = await (
+    await fetch(
+      `https://api.bilibili.com/x/player/pagelist?bvid=${req.query.id}&jsonp=jsonp`
+    )
+  ).json();
+  const text = await (
+    await fetch(`https://api.bilibili.com/x/v1/dm/list.so?oid=${data[0].cid}`)
+  ).text();
+  res.type("application/xml");
+  res.send(text);
+});
+
+app.listen(PORT, () => console.log(PORT));
+
+module.exports = app;
